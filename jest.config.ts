d43121f3f186a9c0e799ee9e0963eb5b43ee38d8
Michael Curtis <michael@sailfin.io>
2024-09-19 21:40:45 -0400

    feat: semantic release

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,24 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  rootDir: "./",
-  testMatch: ["<rootDir>/tests/**/*.spec.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: './',
+  testMatch: ['<rootDir>/tests/**/*.spec.ts'],
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov"],
-  moduleFileExtensions: ["ts", "js", "json"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-  },
+  coveragePathIgnorePatterns: ['node_modules/'],
+  moduleDirectories: ['node_modules', 'src'],
 };
 
 export default config;
